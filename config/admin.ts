1acc225d9740203f85b6e16eec00d58c305d7241
kurka <72251818+MichalJankura@users.noreply.github.com>
2025-03-24 19:51:25 +0100

    Update admin.ts

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }: { env: any }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET',['WioFXrYKyMXhOITKVplKbw==']),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', ['1nMTWrPa/ZQ8+30mnMQAbQ==']),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', ['n9nPkYaEqPIgEo2VeE3vkw==']),
     },
   },
   flags: {
