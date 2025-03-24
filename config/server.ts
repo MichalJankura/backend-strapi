a5fdb8d0c7c8a6e0944c3433d2f00ade6e63121c
kurka <72251818+MichalJankura@users.noreply.github.com>
2025-03-24 20:11:00 +0100

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -2,6 +2,6 @@ export default ({ env }: { env: any }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS', ['hj58Pd+xKz4r8tq9M3YWxA==', 'Bqm8PdXKz4r8tq9M3YWxA==', 'Hjm8Pd+xKz4r8tq9M3YWxA==', 'Kjm8Pd+xKz4r8tq9M3YWxA==']),
+    keys: env('APP_KEYS', ['hj58Pd+xKz4r8tq9M3YWxA==', 'Bqm8PdXKz4r8tq9M3YWxA==', 'Hjm8Pd+xKz4r8tq9M3YWxA==', 'Kjm8Pd+xKz4r8tq9M3YWxA==']).split(','),
   },
 });
