96dd7554bb9d432bf47e3cfc938f6bce8a24f3da
kurka <72251818+MichalJankura@users.noreply.github.com>
2025-04-01 12:09:59 +0200

    Update server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }: { env: any }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 5432),
   app: {
     keys: env('APP_KEYS', ['hj58Pd+xKz4r8tq9M3YWxA==', 'Bqm8PdXKz4r8tq9M3YWxA==', 'Hjm8Pd+xKz4r8tq9M3YWxA==', 'Kjm8Pd+xKz4r8tq9M3YWxA==']).split(','),
   },
