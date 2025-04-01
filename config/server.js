e4458903da4fad676b1bea3ef3352dd7c4d6740d
kurka <72251818+MichalJankura@users.noreply.github.com>
2025-04-01 12:10:19 +0200

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
-// module.exports = ({ env }) => ({
-//     host: env('HOST', '0.0.0.0'),
-//     port: env.int('PORT', 1337),
-//     app: {
-//       keys: env('APP_KEYS', ['hj58Pd+xKz4r8tq9M3YWxA==', 'Bqm8PdXKz4r8tq9M3YWxA==', 'Hjm8Pd+xKz4r8tq9M3YWxA==', 'Kjm8Pd+xKz4r8tq9M3YWxA==']).split(','),
-//     },
-//   });
+module.exports = ({ env }) => ({
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 5432),
+    app: {
+      keys: env('APP_KEYS', ['hj58Pd+xKz4r8tq9M3YWxA==', 'Bqm8PdXKz4r8tq9M3YWxA==', 'Hjm8Pd+xKz4r8tq9M3YWxA==', 'Kjm8Pd+xKz4r8tq9M3YWxA==']).split(','),
+    },
+  });
